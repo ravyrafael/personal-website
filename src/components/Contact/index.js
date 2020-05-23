def93ad27984e6b0dde3174eccfab730c11b4835
ravyrafael <ravyrafael@gmail.com>
2020-05-23 17:19:04 -0300

    Api integration

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,38 +1,33 @@
-import React, {useState} from 'react'
-import sentEmail from "../../utils/sendEmail"
-const Contact = () => {
-  const [form, setForm] = useState({name:"", email:"", subject:"", message:"", phone:""})
+import React, {useState, useEffect} from 'react'
+import api from "../../utils/api"
+import { useForm } from "react-hook-form";
+
+const Contact = ({saveFile}) => {
+  const { handleSubmit, register, errors } = useForm();
   const [warning, setWarning] = useState("")
   const [success, setSuccess] = useState("")
-  const [btnEnabled, setBtn] = useState(false)
+  useEffect(()=>{
+  },[errors])
 
-  const handleInputChange = e => {
-    const {name, value} = e.target
-      setForm({...form,
-          [name]: value
-        })
-  }
-  const SendMessage = async () => {
-    if(validateForm()){
-      await sentEmail()
-      setSuccess("Your message was sent, thank you!")
+
+const onSubmit = (values, e) =>{ 
+  try{
+    api.post("/message",values).then(x=>{
+      console.log(x)
+      e.target.reset()
+      setSuccess("Your message was sent!")
       setWarning("")
-      setForm({name:"", email:"", subject:"", message:"", phone:""})
-    }
-    else
-    {
-      setWarning("Error boy")
-    }
-}
-const validateForm = ()=>{
-  var valid = !Object.entries(form).some((x,i)=>!x[1])
-  console.log(valid)
-  if(valid)
-    if(form.email.includes("@") && form.email.includes(".com"))
-      return true
-  else{
-    return false
-    }
+    }).catch(err=>{
+      console.log(err)
+      setSuccess("")
+      setWarning("Error. Oh boy...")
+    })
+  }
+  catch(err){
+    console.log(err)
+    setSuccess("")
+    setWarning("Error. Oh boy...")
+  }
 }
 
     return (
@@ -48,32 +43,48 @@ const validateForm = ()=>{
       <div className="row">
         <div className="eight columns">
             <fieldset>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div>
+              <red>{errors.name && errors.name.message}</red>
                 <label htmlFor="contactName">Name <span className="required">*</span></label>
-                <input type="text" size={35} id="name" name="name" onChange={handleInputChange} value={form.name}/>
+                <input type="text" size={35} id="name" name="name" ref={register({required:"Required"})}/>
+
               </div>
               <div>
+              <red>{errors.email && errors.email.message}</red>
                 <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-                <input type="email" size={35} id="email" name="email" onChange={handleInputChange} value={form.email} />
+                <input type="email" size={35} id="email" name="email" ref={register({
+                  required: "Required",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                    message: "invalid email address"
+                  }
+                })} />
               </div>
               <div>
-                <label htmlFor="contactSubject">Subject</label>
-                <input type="text" size={35} id="subject" name="subject" onChange={handleInputChange} value={form.subject} />
+              <red>{errors.subject && errors.subject.message}</red>
+                <label htmlFor="contactSubject">Subject <span className="required">*</span></label>
+                <input type="text" size={35} id="subject" name="subject" ref={register({required:"Required"})} />
+                
               </div>
               <div>
-                <label htmlFor="phone">Phone</label>
-                <input type="text" size={35} id="phone" name="phone" onChange={handleInputChange} value={form.phone} />
+              <red>{errors.phone && errors.phone.message}</red>
+                 <label htmlFor="phone">Phone <span className="required">*</span></label>
+                <input type="text" size={35} id="phone" name="phone" ref={register({required:"Required"})} />
+                
               </div>
               <div>
+              <red> {errors.message && errors.message.message}</red>
                 <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                <textarea cols={50} rows={15} id="message" name="message"  onChange={handleInputChange} value={form.contactMessage} />
+                <textarea cols={50} rows={4} id="message" name="message"  ref={register({required:"Required"})} />
               </div>
               <div>
-                <button className="submit" onClick={SendMessage}>Submit</button>
+                <button className="submit">Submit</button>
                 <span id="image-loader">
                   <img alt="" src="images/loader.gif" />
                 </span>
               </div>
+              </form>
             </fieldset>
              {warning && <div id="message-warning"> {warning}</div>}
              {success && <div id="message-success">
